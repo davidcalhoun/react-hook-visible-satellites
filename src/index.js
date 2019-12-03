5c1220552faeb531e82d65eb12d008129cb99ed2
David Calhoun <david.b.calhoun@gmail.com>
2019-12-02 22:19:24 -0500

    export VisibleSatellitesProjection

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 export { default as useVisibleSatellites } from './useVisibleSatellites';
 export { default as VisibleSatellitesTable } from './VisibleSatellitesTable';
+export { default as VisibleSatellitesProjection } from './VisibleSatellitesProjection';
