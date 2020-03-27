8bb7b57867e42d67e9c34968641e32103d1e6117
David Calhoun <david.b.calhoun@gmail.com>
2020-03-27 15:59:57 -0400

    VisibleSatellitesTable: move out some logic into its own SatelliteRow component

--- src/VisibleSatellitesTable.js
+++ src/VisibleSatellitesTable.js
@@ -2,48 +2,66 @@ import React, { useEffect } from "react";
 import PropTypes from "prop-types";
 import useVisibleSatellites from "./useVisibleSatellites";
 import styles from "./VisibleSatellitesTable.css";
-import {uniqBy, path, sort} from "ramda";
-import {toFixedFloat} from "./utils";
+import { uniqBy, path, sort } from "ramda";
+import { toFixedFloat } from "./utils";
+
+const sortByElevation = (a, b) => b.info.elevation - a.info.elevation;
+
+function SatelliteRow(props) {
+	const { info, tleArr } = props;
+	const { elevation, lat, lng, range, velocity, azimuth } = info;
+	const [name] = tleArr;
+
+	return (
+		<tr>
+			<td>{name}</td>
+			<td>{toFixedFloat(elevation)}°</td>
+			<td>{toFixedFloat(azimuth)}°</td>
+			<td>{toFixedFloat(lat, 2)}</td>
+			<td>{toFixedFloat(lng, 2)}</td>
+			<td>{toFixedFloat(range)} km</td>
+			<td>{toFixedFloat(velocity * 60 * 60)} km/h</td>
+			<td>{toFixedFloat(velocity / range, 6)}</td>
+		</tr>
+	);
+}
 
 export default function VisibleSatellitesTable(props) {
-	const {tles, lat, lng} = props;
+	const { tles, lat, lng } = props;
 
-	const [
-		visibleSatellites,
-		{ pause, unpause }
-	] = useVisibleSatellites(tles, lat, lng);
+	const [visibleSatellites, { pause, unpause }] = useVisibleSatellites(
+		tles,
+		lat,
+		lng
+	);
 
-	const uniq = uniqBy(sat => sat.tleArr[0], visibleSatellites);
-
-	const forDisplay = uniq
-		.filter((satellite) => satellite.info.elevation > 20);
-
-	const diff = (a, b) => b.info.elevation - a.info.elevation;
-	const forDisplaySorted = sort(diff, forDisplay);
+	const forDisplay = visibleSatellites.filter(satellite => satellite.info.elevation > 20);
+	const forDisplaySorted = sort(sortByElevation, forDisplay);
 
 	return (
 		<div>
 			<h2>Summary</h2>
 			<ul>
-				<li>{ visibleSatellites.length } above horizon</li>
-				<li>{ forDisplaySorted.length } above 40 degrees</li>
+				<li>{visibleSatellites.length} above horizon</li>
+				<li>{forDisplaySorted.length} above 40 degrees</li>
 			</ul>
 			<table className={styles.container}>
 				<thead>
-					<tr><td>Name</td><td>Elevation</td><td>Azimuth</td><td>Lat</td><td>Lng</td><td>Range</td><td>Velocity</td><td>Speed relative to observer</td></tr>
+					<tr>
+						<td>Name</td>
+						<td>Elevation</td>
+						<td>Azimuth</td>
+						<td>Lat</td>
+						<td>Lng</td>
+						<td>Range</td>
+						<td>Velocity</td>
+						<td>Speed relative to observer</td>
+					</tr>
 				</thead>
 				<tbody>
-					{
-						forDisplaySorted.map(satellite => {
-							const { info, tleArr } = satellite;
-							const { elevation, lat, lng, range, velocity, azimuth } = info;
-							const [name] = tleArr;
-
-							return (
-								<tr key={ name }><td>{ name }</td><td>{ toFixedFloat(elevation) }°</td><td>{ toFixedFloat(azimuth) }°</td><td>{ toFixedFloat(lat, 4) }</td><td>{ toFixedFloat(lng, 4) }</td><td>{ toFixedFloat(range) } km</td><td>{ toFixedFloat(velocity * 60 * 60) } km/h</td><td>{ toFixedFloat(velocity / range, 6) }</td></tr>
-							);
-						})
-					}
+					{forDisplaySorted.map(satellite => {
+						return <SatelliteRow key={satellite.tleArr[1]} {...satellite} />;
+					})}
 				</tbody>
 			</table>
 		</div>
